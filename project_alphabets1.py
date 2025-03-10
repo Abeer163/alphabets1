1488b070d488309aba30ac2652dd2d94201dbe08
Abeer163 <rehmanmughis@gmail.com>
2025-03-10 19:04:11 +0000

    Add files via upload

--- project_alphabets1.py
+++ project_alphabets1.py
@@ -1,6 +1,6 @@
-alphabets = int(input("enter your alphabet: "))
-users_alphabet = "a-z"
-if alphabets >= users_alphabet:
+alphabets = input("enter your alphabet: ")
+users_alphabet = "abcdefghijklmnopqrstuvwxyz"
+if alphabets in users_alphabet:
     print("you have have entered an alphabet")
 else:
     print("you have not entered an alphabet")
